39edb4c10b83ddc97f7ef0f7e79c29b1941b0314
向山　翔 <hms0175km@gmail.com>
2021-12-22 18:56:20 +0900

    [Add]style属性のバインディング

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
+// new Vue({
+//     el: '#app',
+//     data: {
+//         isActive: true,
+//         bg: 'bg-blue'
+//     },
+//     computed: {
+//         classObject: function () {
+//             return {
+//                 red: this.isActive,
+//                 'bg-blue': !this.isActive
+//             }
+//       }  
+//     }
+// })
+
 new Vue({
     el: '#app',
     data: {
-        isActive: true,
-        bg: 'bg-blue'
-    },
-    computed: {
-        classObject: function () {
-            return {
-                red: this.isActive,
-                'bg-blue': !this.isActive
-            }
-      }  
+      styleObject:{
+        color: red,
+        'background-color': blue
+      }
     }
 })
